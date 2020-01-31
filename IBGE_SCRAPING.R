68384dfd830a5606cc8cfd2f5288e7a478605494
Thiago Assis <55933312+thiagoassisk8@users.noreply.github.com>
2020-01-31 13:02:31 -0300

    Add files via upload

--- IBGE_SCRAPING.R
+++ IBGE_SCRAPING.R
@@ -2,20 +2,31 @@ library(tidyverse)
 library(rvest)
 library(bizdays)
 library(lubridate)
+library(openxlsx)
 
 
-#### Carregar dados ####
+### Link de referência dos dados
 
 html_url <- "https://www.ibge.gov.br/estatisticas/economicas/industria/9294-pesquisa-industrial-mensal-producao-fisica-brasil.html?=&t=resultados"
 
 html <- read_html(html_url)
 
+#if(html.matrix !=  ){
+#}
+
 dados <- html %>%
-  html_nodes("tr") %>%
+  #//*[contains(concat( " ", @class, " " ), concat( " ", "table__scroll__container", " " ))]
+  html_nodes(xpath = '//*[@id="table__scroll__container"]') %>%
+  #html_nodes("tr") %>%
   html_text() %>%
   str_replace(",", ".") %>%
-  as.numeric() %>%
-  matrix(ncol = 6, byrow = TRUE) %>%
+  as.character() %>%
+  matrix(ncol = 6, nrow = 28) %>%
   as.data.frame()
+
+
+## Gerar planilha em excel 
+nome_arquivo <- paste0("L:/Area Macro/Base de Dados/Temp/Thiago/", "(", Sys.Date(), ") ", "Dados_IBGE.xlsx")
+write.xlsx(x = dados, file = nome_arquivo)
 
 
